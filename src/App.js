8d44a377cee1bf13bc0112fc01486c8d348d8c8d
Paul B Mahol <onemda@gmail.com>
2019-02-08 14:01:35 +0100

    add picking of 2 colors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 
+var colors = {
+   0: '000',
+   1: '000',
+}
+
 class App extends Component {
+    getColor(number) {
+        let url='http://www.colr.org/json/color/random'
+            fetch(url)
+            .then(function(response) {
+                  return response.json();
+                  })
+        .then(function(myJson) {
+              colors[number] = myJson.new_color;
+              });
+    };
+    componentDidMount() {
+        this.getColor(0);
+        this.getColor(1);
+    };
+
     render() {
         document.body.style.backgroundColor = '#000';
         return (
